cf1c8875602a901a364afac91c984abd52eb1f76
christian <christian@dev.com>
2025-03-19 22:50:13 -0500

    .

---
+++ src/app/gifs/components/gif-list/gif-list.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GifListItemComponent } from "./gif-list-item/gif-list-item.component";
+
+@Component({
+  selector: 'gif-list',
+  imports: [GifListItemComponent],
+  templateUrl: './gif-list.component.html',
+})
+export class GifListComponent {}
--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,8 +1,25 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MenuOption } from '../../../interfaces/menu-option.interface';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
 })
-export class SideMenuOptionsComponent {}
+export class SideMenuOptionsComponent {
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-search',
+      label: 'Buscar',
+      subLabel: 'Busca gifs',
+      route: '/dashboard/search',
+    },
+  ];
+}
